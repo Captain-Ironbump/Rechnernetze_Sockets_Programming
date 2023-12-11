85447e441b68ce7370d1ee8897180f89a8f3d310
Lukas Linde <linde.lukas.99@gmail.com>
2023-12-11 14:39:32 +0100

    finished 4.1 server and client script

--- calculator_client.py
+++ calculator_client.py
@@ -6,25 +6,38 @@ class CalculatorClient:
         self.host = host
         self.port = port
         self.socket = None
+        self.functions_name = {
+            'Summe': 5,
+            'Produkt': 7,
+            'Minimum': 7,
+            'Maximum': 7
+        }
         
     def connect(self):
         # Connect to the server
         self.socket = socket.create_connection((self.host, self.port))
         print('Connected to server')
     
-    def send_calculation(self, operation: str, numbers: [int]):
+    def send_calculation(self, operation: str, numbers: [int]) -> tuple[int, int]:
         # Send a calculation to the server
         # ID: unsigned Int (4 bytes)
         # Operation: example -> "Summe", UTF-8 encoded
         # N: Unsigned Char (1 byte)
         # z1 ... 1N: signed Int (4 bytes)
-        data: bytes = struct.pack('i', 1) + struct.pack('s', operation) + struct.pack('c', len(numbers))
+       
+        if operation not in self.functions_name:
+            raise Exception(f'Unknown operation: {operation}; Operations: {self.functions_name.keys()} are allowed')
+        format_string = str(self.functions_name[operation]) + 's'
+        data: bytes = struct.pack('I', 1) + struct.pack(format_string, operation.encode('UTF-8')) + struct.pack('c', chr(len(numbers)).encode('utf-8'))
         for number in numbers:
             data += struct.pack('i', number)
         self.socket.send(data)
-        print('Sent data')
-        response = self.socket.recv(1024).decode('utf-8')
+        print('Sent data to server with value: ', data)
+        response = self.socket.recv(1024)
         print(f'Response: {response}')
+        result: tuple[int, int] = struct.unpack('<Ii', response)
+        print(f'Result: {result}')
+        return result
         
 if __name__ == '__main__':
     client = CalculatorClient(host='127.0.0.1', port=50000)
--- calculator_server.py
+++ calculator_server.py
@@ -6,10 +6,16 @@ class CalculatorServer:
     def __init__(self, host, port) -> None:
         self._calc = calculator.Calculator()
         self._calculation_functions = {
-            b'Summe': lambda x: self._calc.summation(x),
-            b'Produkt': lambda x: self._calc.product(x),
-            b'Minimum': lambda x: self._calc.minimum(x),
-            b'Maximum': lambda x: self._calc.maximum(x),
+            'Summe': lambda x: self._calc.summation(x),
+            'Produkt': lambda x: self._calc.product(x),
+            'Minimum': lambda x: self._calc.minimum(x),
+            'Maximum': lambda x: self._calc.maximum(x),
+        }
+        self.functions_name = {
+            'Summe': 5,
+            'Produkt': 7,
+            'Minimum': 7,
+            'Maximum': 7
         }
         self.host = host
         self.port = port
@@ -22,31 +28,51 @@ class CalculatorServer:
 
         self.socket.listen(1)
         print(f'Listening on port {self.port}')
-        conn, addr = self.socket.accept() # Accept a connection
-        print(f'Connection from {addr}')
-
+        
         while True:
-            data = conn.recv(1024).decode('utf-8')
-            print(f'Received data: {data}')
-            result = self.calculate(data)
-            conn.send(str(result).encode('utf-8'))
+            conn, addr = self.socket.accept()  # Accept a connection
+            print(f'Connection from {addr}')
 
+            # Handle the connection in a separate function
+            self.handle_connection(conn)
+
+    def handle_connection(self, conn: socket.socket):
+        try:
+            while True:
+                data = conn.recv(1024)
+                print(f'Received data: {data}')
+                if not data:
+                    break
+                result = self.calculate(data)
+                conn.send(result)
+        except Exception as e:
+            print(f"Error handling connection: {e}")
+        finally:
+            conn.close()
+        
 
     def calculate(self, data: bytes) -> bytes:
         # ID: unsigned Int (4 bytes)
         # Operation: example -> "Summe", UTF-8 encoded
         # N: Unsigned Char (1 byte)
         # z1 ... 1N: signed Int (4 bytes)
-        operation_buffer = 7 
-        message_id = struct.unpack('i', data[:4])
-        operation_first = struct.unpack('c', data[4:5])
-        if operation_first == 'S':
-            operation_buffer = 5
-        operation = struct.unpack('s', data[4:operation_buffer+4])
-        numbers = struct.unpack('c', data[operation_buffer+4:operation_buffer+5])
+
+        operation = data.decode('utf-8')
+        # Define a set of valid characters
+        valid_chars = set("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789")
+        # Use a loop to filter out unwanted characters
+        operation = ''.join(char for char in operation if char in valid_chars)
+        print(f'Operation: {operation}')
+        if operation not in self.functions_name:
+            raise Exception(f'Unknown operation: {operation}; Operations: {self.functions_name.keys()} are allowed')
+
+        message_id = struct.unpack('I', data[:4])[0]
+        number = struct.unpack('c', data[self.functions_name[operation]+4:self.functions_name[operation]+5])
+        number = struct.unpack('B', number[0])[0]
         number_list = []
-        for i in range(numbers):
-            number_list.append(struct.unpack('i', data[operation_buffer+5+i*4:operation_buffer+9+i*4]))
+        print(number)
+        for index in range(number):
+            number_list.append(struct.unpack('i', data[self.functions_name[operation]+5+index*4:self.functions_name[operation]+9+index*4])[0])
 
         calc_result = 0
         if operation in self._calculation_functions:
@@ -54,7 +80,7 @@ class CalculatorServer:
         else:
             raise Exception(f'Unknown operation: {operation}')
 
-        packed_result = struct.pack('i', message_id) + struct.pack('i', calc_result)
+        packed_result = struct.pack('I', message_id) + struct.pack('i', calc_result)
         return packed_result
 
 if __name__ == '__main__':
